d8cb2b2222e56bb055b3166d84aeadc3c38b2329
trieule50 <trieule50@gmail.com>
2021-08-13 14:45:34 -0400

    Edit links for project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -18,7 +18,7 @@ const Projects = () => {
                         <Card.Text>
                             Has there ever been a situation where you were unsure how a sentence or paragraph is perceived by others? With the help of this app, Speech Audit, and IBM's Speech to Text and Tone Analyzer API, users are able to see what emotion others may perceive in your sentence!
                         </Card.Text>
-                        <div>
+                        <div className='links'>
                             <a href="https://speechaudit.netlify.app/" target="_blank">
                             <Button variant="dark" ><ExitToAppIcon/>To Deploy Link</Button>
                             </a>
@@ -35,7 +35,7 @@ const Projects = () => {
                         <Card.Text>
                             A working, an interactive, full-stack application that receives data from a custom API that my team designed. DevOgraphy allows users to learn more about programming languages, their icons, their authors, and their creation date.
                         </Card.Text>
-                        <div>
+                        <div className='links'>
                             <a href="https://devography.netlify.app/" target="_blank">
                             <Button variant="dark"><ExitToAppIcon/>To Deploy Link</Button>
                             </a>
@@ -52,7 +52,7 @@ const Projects = () => {
                         <Card.Text>
                             Cook Assistant is working, interactive, React application that received data from Edamam, the third-party API. The app allows users to find a recipe that fits their dietary needs and search for any key ingredient that the user wants in the recipe. Click on the link below and start cooking!
                         </Card.Text>
-                        <div>
+                        <div className='links'>
                             <a href="https://cook-assistant.netlify.app/" target="_blank">
                             <Button variant="dark"><ExitToAppIcon/>To Deploy Link</Button>
                             </a>
@@ -69,7 +69,7 @@ const Projects = () => {
                         <Card.Text>
                             Snake was first launched in 1997 on the Nokia 6110 and became the second mobile game after Tetris. Frantic Engineer was inspired by the Snake game and my previous career. Frantic Engineer is an engineer who is getting ready to bid on a project. However, in order to submit the bid on time, all the proposal sheets will need to be collected. Can you help the engineer in submitting the bid proposal on time?
                         </Card.Text>
-                        <div>
+                        <div className='links'>
                             <a href="https://trieule50.github.io/frantic-engineer-project1/" target="_blank">
                             <Button variant="dark"><ExitToAppIcon/>To Deploy Link</Button> 
                             </a>
